888e86f8b5cea61f25409bc87487272e1e097b37
maxiehernandez <maxinehernandez@hotmail.com>
2017-01-26 17:51:54 -0500

    updated routes and made proper link-to connections

--- app/router.js
+++ app/router.js
@@ -8,13 +8,18 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('posts', {path: '/'}, function() {
     this.route("new");
-  });
-  this.route('post', {path: '/post/:post_id'}, function() {
-    this.route('edit');
-    this.route('comments', function() {
-      this.route('new');
+    this.route('show', {path: '/post/:post_id'}, function() {
+      this.route('comments', function() {
+        this.route('new');
+      });
     });
+    this.route('edit', {path: '/post/edit/:post_id'});
   });
+  // this.route('post', {path: '/post/:post_id'}, function() {
+  //   this.route('comments', function() {
+  //     this.route('new');
+  //   });
+  // });
 });
 
 export default Router;
