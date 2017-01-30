e915e29f3a438145c75daba4b2d3c72c432fc757
Maxie Hernandez <maxinehernandez@hotmail.com>
2017-01-30 01:00:01 -0500

    WIP adding edit feature

---
+++ app/components/update-post.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service("store"),
+  actions: {
+    updatePost(image, name, caption) {
+      let model = {
+        imageUrl: image,
+        name: name,
+        caption: caption
+      }
+
+      this.get('name');
+      this.get('imageUrl');
+      this.get('caption');
+
+      this.set('name', "");
+      this.set('imageUrl', "");
+      this.set('caption', "");
+      
+      this.sendAction('updatePost', model);
+    }
+  }
+});
--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').find('post', params.post_id);
+  },
+  actions: {
+    updatePost: function(model) {
+      let post = this.get('store').createRecord("post", {
+        name: model.name,
+        imageUrl: model.imageUrl,
+        caption: model.caption
+      });
+      post.save();
+      this.transitionTo('posts')
+    }
   }
 });
