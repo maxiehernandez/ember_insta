59b0b9f6bdaff8da18b7c40cefd270ac5d805358
maxiehernandez <maxinehernandez@hotmail.com>
2017-01-31 18:01:21 -0500

    trying upload feature

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,6 +2,7 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
