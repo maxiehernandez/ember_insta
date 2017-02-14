ef908e25f55b1c7d08d325c6df3dc529d75290eb
Maxie <maxinehernandez@hotmail.com>
2017-02-14 13:46:57 -0500

    made changes to work with new rails app

--- app/models/post.js
+++ app/models/post.js
@@ -6,11 +6,11 @@ export default DS.Model.extend({
   // name: DS.attr('string'),
   // caption: DS.attr('string')
   comments: DS.hasMany('comments', {async:true}),
-  // users: DS.hasMany('users', {async:true}),
+  user: DS.belongsTo('user', {async:true}),
   caption: DS.attr('string'),
-  originalFileName: DS.attr('string'),
+  // originalFileName: DS.attr('string'),
   fileName: DS.attr('string'),
-  fileContentType: DS.attr('string'),
-  fileUpdatedAt: DS.attr('date'),
+  // fileContentType: DS.attr('string'),
+  // fileUpdatedAt: DS.attr('date'),
   imageUrl: DS.attr('string')
 });
--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
+    let user = this.get('store').peekRecord("user", 1);
     return this.get('store').createRecord("post", {
+      user: user,
       fileName: null,
       imageUrl: null,
       caption: null
