874fe6a7a4d20f3f9e7b611c853dc6eccf784823
Maxie <maxinehernandez@hotmail.com>
2017-02-14 10:20:29 -0500

    updated post attributes

--- app/models/post.js
+++ app/models/post.js
@@ -1,8 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  // comments: DS.hasMany('comments', {async:true}),
+  // imageUrl: DS.attr('string'),
+  // name: DS.attr('string'),
+  // caption: DS.attr('string')
   comments: DS.hasMany('comments', {async:true}),
-  imageUrl: DS.attr('string'),
-  name: DS.attr('string'),
-  caption: DS.attr('string')
+  // users: DS.hasMany('users', {async:true}),
+  caption: DS.attr('string'),
+  originalFileName: DS.attr('string'),
+  fileName: DS.attr('string'),
+  fileContentType: DS.attr('string'),
+  fileUpdatedAt: DS.attr('date'),
+  imageUrl: DS.attr('string')
 });
