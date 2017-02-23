bb9b7eb973e287390e856b764f70b799f53f3c54
Maxie <maxinehernandez@hotmail.com>
2017-02-23 18:10:06 -0500

    WIP - pagination

--- app/routes/posts/index.js
+++ app/routes/posts/index.js
@@ -1,7 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('post');
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    size: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    return this.store.query('post', { page: {
+        number: params.page,
+        size: params.size
+      }
+    });
   }
+  // model() {
+  //   return this.store.findAll('post');
+  // }
 });
