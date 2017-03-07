d47f7b57d832c30f8b42b3e4158ee7d23e152aeb
Maxie <maxinehernandez@hotmail.com>
2017-03-07 18:05:40 -0500

    WIP - if authenticated

--- app/services/session.js
+++ app/services/session.js
@@ -1,24 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  currentUser: null,
 
-  init() {
+  isAuthenticated: function() {
     const service = this;
-    Ember.$.ajax({
-      url: 'http://localhost:3000/users/current',
-      method: "GET",
-      success: function(data) {
-        if (data) {
-          let currentUser = data.data;
-          service.set("currentUser", currentUser);
+    const store = this.get("store");
+    if (this.get("currentUser")) {
+      console.log('currentUser', "im in the if statement");
+      return true;
+    } else {
+      console.log('currentUser', "im in the else statement");
+      Ember.$.ajax({
+        url: 'http://localhost:3000/users/current',
+        method: "GET",
+        async:true,
+        success: function(data) {
+          if (data) {
+            const currentUser = store.push(store.normalize("user",data.data));
+            service.set("currentUser", currentUser);
+            service.notifyPropertyChange("currentUser");
+          }
+        },
+        error: function(error) {
+          if (error) {
+            console.log(error);
+          }
         }
-      },
-      error: function(error) {
-        if (error) {
-          console.log(error);
-        }
-      }
-    })
-  }
+      });
+    }
+  }.property('currentUser'),
+  currentUser: null
 });
