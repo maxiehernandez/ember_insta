e2186eee60408b35e290c0529fff0bf11389ca8d
Maxie <maxinehernandez@hotmail.com>
2017-03-08 16:30:53 -0500

    Updated session service to run on isAuthenticated

--- app/initializers/session.js
+++ app/initializers/session.js
@@ -2,6 +2,7 @@ export function initialize(application) {
   application.inject('route', 'session', 'service:session');
   application.inject('controller', 'session', 'service:session');
   application.inject('component', 'session', 'service:session');
+  application.inject('store', 'session', 'service:session');
 }
 
 export default {
--- app/services/session.js
+++ app/services/session.js
@@ -1,24 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  store: Ember.inject.service('store'),
 
   isAuthenticated: function() {
     const service = this;
-    const store = this.get("store");
+    const store = this.get('store');
     if (this.get("currentUser")) {
-      console.log('currentUser', "im in the if statement");
       return true;
     } else {
-      console.log('currentUser', "im in the else statement");
       Ember.$.ajax({
         url: 'http://localhost:3000/users/current',
         method: "GET",
         async:true,
         success: function(data) {
           if (data) {
-            const currentUser = store.push(store.normalize("user",data.data));
-            service.set("currentUser", currentUser);
-            service.notifyPropertyChange("currentUser");
+            const currentUser = store.push(store.normalize("user", data.data));
+            const current_user = service.set("currentUser", currentUser);
           }
         },
         error: function(error) {
